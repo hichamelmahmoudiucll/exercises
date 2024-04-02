90b8342eecd5ca512d2e10b420ba14f72bfe6ea2
hichamelmahmoudiucll <145760365+hichamelmahmoudiucll@users.noreply.github.com>
2024-04-02 15:04:11 +0200

    Update student.py

--- 01-basic-python/04-lists/07-target-sum/student.py
+++ 01-basic-python/04-lists/07-target-sum/student.py
@@ -1,3 +1,9 @@
 # Write your code here
 def target_sum(ns, target):
-     
+    for x in ns:
+          for y in ns:
+               if (x + y == target):
+                    return True
+    return False
+               
+    
